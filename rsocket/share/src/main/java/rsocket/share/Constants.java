e647462186b3e138396d452e0c40cef2c4b0e8d0
duckhanh00 <duckhanhheli@gmail.com>
2023-03-15 17:31:49 +0700

    delete file

--- rsocket/share/src/main/java/rsocket/share/Constants.java
+++
@@ -1,8 +0,0 @@
-package rsocket.share;
-
-public class Constants {
-    public static final String MIME_FILE_EXTENSION = "message/x.upload.file.extension";
-    public static final String MIME_FILE_NAME = "message/x.upload.file.name";
-    public static final String FILE_NAME = "file-name";
-    public static final String FILE_EXTN = "file-extn";
-}
--- rsocket/share/src/main/java/rsocket/share/Status.java
+++
@@ -1,7 +0,0 @@
-package rsocket.share;
-
-public enum Status {
-    CHUNK_COMPLETED,
-    COMPLETED,
-    FAILED
-}
